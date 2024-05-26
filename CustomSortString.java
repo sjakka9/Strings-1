742429af78be2089a5558423989b7dc7bbcad30a
sjakka9 <jakkasushmitha18@gmail.com>
2024-05-26 13:02:52 -0400

    Code Changes

---
+++ CustomSortString.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+class Solution {
+    public String customSortString(String order, String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<order.length(); i++)
+        {
+            char c = order.charAt(i);
+            if(map.containsKey(c))
+            {
+                int cnt = map.get(c);
+                for(int k =0; k<cnt; k++)
+                {
+                    result.append(c);
+                }
+                map.remove(c);
+            }
+        }
+
+        for(char c : map.keySet())
+        {
+            int cnt = map.get(c);
+            for(int k =0; k<cnt; k++)
+            {
+                result.append(c);
+            }
+            //map.remove(c);
+        }
+        return result.toString();
+    }
+}
